df0463af286253bb17ee88dcef4facd04d67718d
Abhay <abhaysingbhosale1@gmail.com>
2024-07-04 15:10:19 +0530

    Fixed #3 - Fixed Status Bar Color to Match App Background 📱

--- app/src/main/java/dev/abhaycloud/fdtracker/presentation/theme/Theme.kt
+++ app/src/main/java/dev/abhaycloud/fdtracker/presentation/theme/Theme.kt
@@ -9,7 +9,11 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = primaryDark,
@@ -80,6 +84,16 @@ fun FDTrackerTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.background.toArgb() // change color status bar here
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+        }
+    }
+
 
     MaterialTheme(
         colorScheme = colorScheme,
