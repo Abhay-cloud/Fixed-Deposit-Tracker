6a1de5114e687d8effba9e9f6b4091d98e76b594
Abhay <abhaysingbhosale1@gmail.com>
2024-07-04 13:18:46 +0530

    Closed #2 - Added serial numbers to CSV file for fixed deposits 📑

--- app/src/main/java/dev/abhaycloud/fdtracker/domain/usecase/ExportFixedDepositUseCase.kt
+++ app/src/main/java/dev/abhaycloud/fdtracker/domain/usecase/ExportFixedDepositUseCase.kt
@@ -12,10 +12,10 @@ class ExportFixedDepositUseCase @Inject constructor(private val repository: Fixe
         val fixedDeposits = repository.getAllFixedDeposits()
         val totalInvestedAmount = repository.getTotalInvestedAmount().firstOrNull()
         val totalMaturityAmount = repository.getTotalMaturityAmount().firstOrNull()
-        val header = "ID, Bank Name, Principal Amount, Maturity Amount, Interest Rate, Start Date, Maturity Date, Notes, CreatedAt"
-        val csvData = fixedDeposits.firstOrNull()?.joinToString(separator = "\n") { fd ->
-            "${fd.id}, ${fd.bankName}, ${fd.principalAmount}, ${fd.maturityAmount}, ${fd.interestRate}, \"${fd.startDate.time.toDateString()}\", \"${fd.maturityDate.time.toDateString()}\", ${fd.notes}, \"${fd.createdAt.time.toDateString()}\""
-        }
+        val header = "Sr. No, ID, Bank Name, Principal Amount, Maturity Amount, Interest Rate, Start Date, Maturity Date, Notes, CreatedAt"
+        val csvData = fixedDeposits.firstOrNull()?.mapIndexed { index, fd ->
+            "${index + 1}, ${fd.id}, ${fd.bankName}, ${fd.principalAmount}, ${fd.maturityAmount}, ${fd.interestRate}, \"${fd.startDate.time.toDateString()}\", \"${fd.maturityDate.time.toDateString()}\", ${fd.notes}, \"${fd.createdAt.time.toDateString()}\""
+        }?.joinToString(separator = "\n")
         val footer = "Invested Amount:, RS. $totalInvestedAmount, Maturity Amount:, RS. $totalMaturityAmount"
         Log.d("myapp", "CSV: $csvData")
         return "$header\n${csvData ?: ""}\n\n$footer"
